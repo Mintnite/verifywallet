c1ab7d7b3fb2580e8c7b3eb13c71cc8ff34165ae
mwhc00 <melvinwonghc@gmail.com>
2023-07-14 07:21:10 +0900

    Updates

---
+++ js/env.js
@@ -0,0 +1,4 @@
+const walletURLPrefix  = "https://metamask.app.link/dapp/https://verify.mintnite.io/sign.html";
+const fetchURL = "https://mn.test/api/v1/is-verified";
+const signURL = "https://verifywallet.test/sign.html";
+const verifyURL = 'https://mintnite.io/api/v1/verify?';
